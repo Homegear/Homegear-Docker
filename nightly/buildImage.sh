7d2b35cd62c575ec1a4de98fd695961660a699d3
sathya <sathya@build.dataserver.eu>
2017-01-08 14:09:17 +0100

    Moved latest tag to stable

--- nightly/buildImage.sh
+++ nightly/buildImage.sh
@@ -1,3 +1,2 @@
 #/bin/sh
 docker build --no-cache -t homegear/homegear:nightly .
-docker tag homegear/homegear:nightly homegear/homegear:latest
--- stable/buildImage.sh
+++ stable/buildImage.sh
@@ -1,2 +1,3 @@
 #/bin/sh
 docker build --no-cache -t homegear/homegear:stable .
+docker tag homegear/homegear:nightly homegear/homegear:latest
