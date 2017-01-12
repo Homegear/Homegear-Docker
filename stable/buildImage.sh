1e500ba560e42de49be087189a2528c5e9362591
Sathya Laufer <sathya@laufers.net>
2017-01-12 20:59:37 +0100

    Fixed: homegear:stable now is latest

--- stable/buildImage.sh
+++ stable/buildImage.sh
@@ -1,3 +1,3 @@
 #/bin/sh
 docker build --no-cache -t homegear/homegear:stable .
-docker tag homegear/homegear:nightly homegear/homegear:latest
+docker tag homegear/homegear:stable homegear/homegear:latest
