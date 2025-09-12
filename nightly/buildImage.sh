ebfbe75820f540f6b22e9687b4cc0d2cc5512152
Dr. Sathya Laufer <s.laufer@homegear.email>
2025-09-12 22:00:51 +0200

    Small chnages

--- nightly/buildImage.sh
+++ nightly/buildImage.sh
@@ -1,3 +1,3 @@
 #/bin/sh
-cd $(dirname "$(realpath "$0")")/..
+cd $(dirname "$(realpath "$0")")/../
 docker buildx build -f nightly/Dockerfile --no-cache --platform linux/amd64,linux/arm64,linux/arm/v7 --tag homegear/homegear:nightly --push .
--- stable/buildImage.sh
+++ stable/buildImage.sh
@@ -1,3 +1,3 @@
 #/bin/sh
-cd ..
+cd $(dirname "$(realpath "$0")")/../
 docker buildx build -f stable/Dockerfile --no-cache --platform linux/amd64,linux/arm64,linux/arm/v7 --tag homegear/homegear:stable --tag homegear/homegear:latest --push .
